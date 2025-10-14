078364c2e5a6946038b7b1e3abc1097c427f169d
Fichiii <kevinehrlich31@gmail.com>
2025-10-14 21:34:58 +0200

    Übung 1 hinzugefügt

---
+++ Übung1/Fraction/src/Helper.java
@@ -0,0 +1,10 @@
+public class Helper {
+    public static int ggT(int a, int b){
+    while (b != 0) {
+        int rest = a % b; 
+        a = b;            
+        b = rest;         
+    }
+    return a; 
+    }
+}
